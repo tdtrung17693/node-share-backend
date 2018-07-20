c55595193e7bb46326763138236d4a57a9038859
Tran Dinh Trung <trandinhtrung176@gmail.com>
2018-07-20 14:43:30 +0700

    update config

--- src/config/misc.js
+++ src/config/misc.js
@@ -1,11 +1,12 @@
 const PORT = process.env.PORT || 3000
-const ROOT = process.env.ROOT_URI || 'localhost'
+const ROOT = process.env.ROOT_URL || 'localhost:3000'
+const WEB_ROOT = process.env.WEB_ROOT || 'localhost:3001'
 
 export default {
-    apiRoot: `${ROOT}:${PORT}/api`,
+    apiRoot: `${ROOT}/api`,
     port: PORT,
-    root: `${ROOT}:${PORT}`,
-    webRoot: `${ROOT}:3001`,
+    root: `${ROOT}`,
+    webRoot: `${WEB_ROOT}`,
     adminEmail: process.env.ADMIN_EMAIL,
     adminName: process.env.ADMIN_NAME
 }
